165ad71bcb6808d6c6aeab504ad54530609a81a6
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-18 21:02:30 -0600

    Start of the project

---
+++ src/main/java/com/fmj/followmyjob/FollowMyJobApplication.java
@@ -0,0 +1,13 @@
+package com.fmj.followmyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowMyJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowMyJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmj/followmyjob/FollowMyJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmj.followmyjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowMyJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
