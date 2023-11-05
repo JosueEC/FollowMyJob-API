6738d56ec64d86e4eb254ebdb5965197cdfa1512
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-04 21:16:58 -0600

    feat(repository): 🔨 Add the UserRepository

--- src/main/java/com/fmj/followmyjob/user/respository/UserRepository.java
+++ src/main/java/com/fmj/followmyjob/user/respository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.fmj.followmyjob.user.respository;
 
-import com.fmj.followmyjob.user.models.User;
+import com.fmj.followmyjob.user.entity.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
