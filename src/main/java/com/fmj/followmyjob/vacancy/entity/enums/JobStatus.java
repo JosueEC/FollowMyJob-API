20aa76863b30c874a7d58cc36f388e7cf27d96cc
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-07 16:49:37 -0600

    feat(vacancy/entity): 🔨 Add the 'Vacancy' entity with JPA annotations, column constraints and lombok annotations

---
+++ src/main/java/com/fmj/followmyjob/vacancy/entity/enums/JobStatus.java
@@ -0,0 +1,16 @@
+package com.fmj.followmyjob.vacancy.entity.enums;
+
+/*
+ * Un enum, es la abreviacion para "enumeracion", nos permite definir
+ * objetos que enumeran valores ya definidos, por ejemplo el enum
+ * "DiaDeLaSemana" el cual tendra enumerados los dias de la semana
+ */
+public enum JobStatus {
+    PENDING,
+    APPLIED,
+    NO_RESPONSE,
+    INTERVIEW,
+    TECHNICAL_TEST,
+    OFFER,
+    REJECTED
+}
