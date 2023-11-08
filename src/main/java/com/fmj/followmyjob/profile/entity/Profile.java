842d8ae21ab15f7b41ba9b6a5438ebe2ea0a37bf
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-07 20:21:16 -0600

    feat(Profile entity): 🔨 Add the @OneToMany relationship between the Profile entity and the Network entity

--- src/main/java/com/fmj/followmyjob/profile/entity/Profile.java
+++ src/main/java/com/fmj/followmyjob/profile/entity/Profile.java
@@ -1,10 +1,16 @@
 package com.fmj.followmyjob.profile.entity;
 
+import java.util.List;
 import java.util.UUID;
 
+import com.fmj.followmyjob.network.entity.Network;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,4 +40,12 @@ public class Profile {
         nullable = false
     )
     private String profession;
+
+    @OneToMany(
+        targetEntity = Network.class,
+        cascade = CascadeType.ALL,
+        fetch = FetchType.LAZY,
+        mappedBy = "profile"
+    )
+    private List<Network> networks;
 }
