adec13e5c7ebc417c87028811fb0760c55f545de
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-07 20:20:21 -0600

    feat(UsersOccupations entity): 🔨 Add the @ManyToOne relationship between the UsersOccupations entity with the User entity and the Occupation entity

--- src/main/java/com/fmj/followmyjob/occupation/entity/UsersOccupations.java
+++ src/main/java/com/fmj/followmyjob/occupation/entity/UsersOccupations.java
@@ -1,10 +1,14 @@
 package com.fmj.followmyjob.occupation.entity;
 
+import com.fmj.followmyjob.user.entity.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,4 +38,22 @@ public class UsersOccupations {
         nullable = false
     )
     private Integer monthsExperience;
+
+    @ManyToOne(
+        targetEntity = User.class
+    )
+    @JoinColumn(
+        name = "user_id",
+        referencedColumnName = "id"
+    )
+    private User user;
+
+    @ManyToOne(
+        targetEntity = Occupation.class
+    )
+    @JoinColumn(
+        name = "occupation_id",
+        referencedColumnName = "id"
+    )
+    private Occupation occupation;
 }
