3edec0cd08d1c06d95f0693ab9a1d96c7332745c
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-07 20:12:45 -0600

    feat(Color.java): 🔨 Add the @OneToMany relationship between the Color entity and the Vacancy entity

--- src/main/java/com/fmj/followmyjob/color/entity/Color.java
+++ src/main/java/com/fmj/followmyjob/color/entity/Color.java
@@ -1,10 +1,16 @@
 package com.fmj.followmyjob.color.entity;
 
+import java.util.List;
+
+import com.fmj.followmyjob.vacancy.entity.Vacancy;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,4 +35,11 @@ public class Color {
         unique = true
     )
     private String name;
+
+    @OneToMany(
+        targetEntity = Vacancy.class,
+        fetch = FetchType.LAZY,
+        mappedBy = "color"
+    )
+    private List<Vacancy> vacancies;
 }
