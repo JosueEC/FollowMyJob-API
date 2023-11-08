a81cded3cd74ff71626a49b189cc53415d02e549
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-07 20:14:55 -0600

    feat(Company.java): 🔨 Add the @OneToMany relationship between the Company entity and the Vacancy entity and the Network entity

--- src/main/java/com/fmj/followmyjob/company/entity/Company.java
+++ src/main/java/com/fmj/followmyjob/company/entity/Company.java
@@ -1,10 +1,17 @@
 package com.fmj.followmyjob.company.entity;
 
+import java.util.List;
 import java.util.UUID;
 
+import com.fmj.followmyjob.network.entity.Network;
+import com.fmj.followmyjob.vacancy.entity.Vacancy;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +34,19 @@ public class Company {
         nullable = false
     )
     private String name;
+
+    @OneToMany(
+        targetEntity = Network.class,
+        cascade = CascadeType.ALL,
+        fetch = FetchType.LAZY,
+        mappedBy = "company"
+    )
+    private List<Network> networks;
+
+    @OneToMany(
+        targetEntity = Vacancy.class,
+        fetch = FetchType.LAZY,
+        mappedBy = "company"
+    )
+    private List<Vacancy> vacancies;
 }
